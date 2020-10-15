0dc3ba4d0d427b16421b4c4b40c52fcdee7495ce
BabuJoseph <babu.joseph5757@gmail.com>
2020-10-15 18:18:09 +0530

    creating pull request

--- alpha.sh
+++ alpha.sh
@@ -3,3 +3,7 @@ Does not allow overwriting each other’s changes.
 Maintains a history of every version.
 Security
 No need of powerful hardware
+
+creating pull request
+Git is easier branching
+Git is a security
