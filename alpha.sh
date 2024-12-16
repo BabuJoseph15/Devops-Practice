9fbc055e670236fd33f0122b88a1d197cba183bf
Babu Guduru <bguduru@altimetrik.com>
2024-12-16 14:52:07 +0530

    committing Design Changes

--- alpha.sh
+++
@@ -1,9 +0,0 @@
-Allows developers to work simultaneously.
-Does not allow overwriting each other’s changes.
-Maintains a history of every version.
-Security
-No need of powerful hardware
-
-creating pull request
-Git is easier branching
-Git is a security
